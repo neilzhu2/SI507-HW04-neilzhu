c68ddf804278dc2591fedbbb6c7272bb81608db3
neilzhu2 <neilzhu@umich.edu>
2018-01-27 17:21:40 -0500

    last10

--- magic_eight.py
+++ magic_eight.py
@@ -16,24 +16,24 @@ def ask_8_ball():
 
   if isNotQ == 0:
 		number=randrange(1,21,1)
-		if(number == 1):
-			print("It is certain")
-		elif(number == 2):
-			print("It is decidedly so")
-		elif(number == 3):
-			print("Without a doubt")
-		elif(number == 4):
-			print("Yes definitely")
-		elif(number == 5):
-			print("You may rely on it")
-		elif(number == 6):
-			print("As I see it, yes")
-		elif(number == 7):
-			print("Most likely")
-		elif(number == 8):
-			print("Outlook good")
-		elif(number == 9):
-			print("Yes")
-		elif(number == 10):
-			print("Signs point to yes")
+		if(number == 11):
+			print("Reply hazy try again")
+		elif(number == 12):
+			print("Ask again later")
+		elif(number == 13):
+			print("Better not tell you now")
+		elif(number == 14):
+			print("Cannot predict now")
+		elif(number == 15):
+			print("Concentrate and ask again")
+		elif(number == 16):
+			print("Don't count on it")
+		elif(number == 17):
+			print("My reply is no")
+		elif(number == 18):
+			print("My sources say no")
+		elif(number == 19):
+			print("Outlook not so good")
+		elif(number == 20):
+			print("Very doubtfu")
 
