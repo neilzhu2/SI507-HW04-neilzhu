14daec0d115d53ab1ef8d3f04091f78659c4873b
neilzhu2 <neilzhu@umich.edu>
2018-01-27 16:42:25 -0500

    check

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,15 @@
 def ask_8_ball():
+  isNotQ = 1
+
+  while isNotQ:	
     question = input("What's your Y/N question?")
+
+    if question != "quit":
+    	if question[-1] == "?":
+    		isNotQ = 0
+    		return question
+    	else:
+    		print("I’m sorry, I can only answer questions.")
+
+    else:
+    	break
